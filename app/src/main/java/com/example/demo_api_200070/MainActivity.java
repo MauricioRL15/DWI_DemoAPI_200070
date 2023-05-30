f436b06f227041004058fe5f9b93552d5d500bf1
MauricioRL15 <15rlmauricio@gmail.com>
2023-05-30 16:27:49 -0600

    Clase 30 de Mayo

--- app/src/main/java/com/example/demo_api_200070/MainActivity.java
+++ app/src/main/java/com/example/demo_api_200070/MainActivity.java
@@ -4,20 +4,99 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnGuardar ;
+    private Button btnGuardar;
+    private Button btnBuscar;
+    private Button btnActualizar;
+    private Button btnEliminar;
+    private EditText etCodigoBaras;
+    private EditText etDescripcion;
+    private EditText etMarca;
+    private EditText etPrecioCompra;
+    private EditText etPrecioVenta;
+    private EditText etExistencia;
+    private ListView lvProductos;
+    private RequestQueue colaPeticiones;
+    private JsonArrayRequest jsonArrayRequest;
+    private ArrayList<String> origenDatos = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        colaPeticiones = Volley.newRequestQueue(this);
         btnGuardar = findViewById(R.id.btnGuardar);
+        btnActualizar = findViewById(R.id.btnActualizar);
+        btnBuscar = findViewById(R.id.btnBuscar);
+        btnEliminar = findViewById(R.id.btnEliminar);
+        etCodigoBaras = findViewById(R.id.etCodigoBaras);
+        etDescripcion = findViewById(R.id.etDescripcion);
+        etMarca = findViewById(R.id.etMarca);
+        etPrecioCompra = findViewById(R.id.etPrecioCompra);
+        etPrecioVenta = findViewById(R.id.etPrecioVenta);
+        etExistencia = findViewById(R.id.etExistencia);
+        lvProductos = findViewById(R.id.lvProductos);
+        listarProductos();
+
+
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //listarProductos();
             }
         });
+    }
+    protected void listarProductos(){
+        String url = "http://10.10.62.17:3300";
+        jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i = 0 ; i<response.length();i++){
+                            try {
+                                String descripcion = response.getJSONObject(i).getString("descripcion");
+                                String marca = response.getJSONObject(i).getString("marca");
+                                origenDatos.add(descripcion+" -> "+marca);
+                            } catch (JSONException e) {
+
+                            }
+                        }
+                        adapter = new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, origenDatos);
+                        lvProductos.setAdapter(adapter);
+                    }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        colaPeticiones.add(jsonArrayRequest);
     }
 }
