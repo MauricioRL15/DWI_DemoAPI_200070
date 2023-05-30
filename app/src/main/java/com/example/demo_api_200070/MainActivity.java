38018d50c681cc7eea30377be1b7bc451374f871
MauricioRL15 <15rlmauricio@gmail.com>
2023-05-29 19:08:04 -0600

    Segundo Commit

--- app/src/main/java/com/example/demo_api_200070/MainActivity.java
+++ app/src/main/java/com/example/demo_api_200070/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.demo_api_200070;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnGuardar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGuardar = findViewById(R.id.btnGuardar);
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
